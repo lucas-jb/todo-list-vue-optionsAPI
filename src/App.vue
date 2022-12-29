f9268052a291bfc27d1d3be2db2497d089645de4
lucas-jb <lucasjuarezbarrios@gmail.com>
2022-12-30 00:43:43 +0100

    create alert component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <nav class="navbar">
-    <img src="./assets/mono.png" width="50"/>
+    <img src="./assets/mono.png" width="50" />
     <div class="brand">Todo list App</div>
   </nav>
+
   <main class="container">
+    <Alert
+      message="Todo title is required"
+      :show="showAlert"
+      @close="showAlert = false"
+      type="danger"
+    ></Alert>
     <section>
       <form class="add-todo-form">
-        <input v-model="todoTitle" type="text" placeholder="Todo title"/>
+        <input v-model="todoTitle" type="text" placeholder="Todo title" />
         <div>
           <button @click.prevent="addTodo">Add Todo</button>
         </div>
@@ -14,10 +21,11 @@
     </section>
 
     <section>
-      <div v-for="(todo, i) in todos" class="todo">
-        <p>{{ todo }}</p>
+      <div v-for="todo in todos" class="todo" :key="todo.id">
+        <p>{{ todo.title }}</p>
         <div>
-          <button @click.prevent="removeTodo(i)" class="remove-todo-btn">x</button>
+          <button @click="editTodo(todo)" class="edit-todo-btn">e</button>
+          <button @click="removeTodo(todo)" class="remove-todo-btn">x</button>
         </div>
       </div>
     </section>
@@ -25,24 +33,43 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        todoTitle: "",
-        todos: [],
+import Alert from "./components/Alert.vue";
+
+export default {
+  components: {
+    Alert,
+  },
+  data() {
+    return {
+      todoTitle: "",
+      todos: [],
+      showAlert: false,
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.todoTitle !== "") {
+        this.todos.push({
+          title: this.todoTitle,
+          id: this.todos.length + Math.floor(Math.random() * 100),
+        });
+        this.todoTitle = "";
+      } else {
+        this.showAlert = true;
       }
     },
-    methods: {
-      addTodo(){
-        this.todos.push(this.todoTitle)
-      },
-      removeTodo(todoTitle){
-        this.todos = this.todos.filter((todo, i) => i !== todoTitle)
-      }
-    }
-  }
-  
 
+    removeTodo(todo) {
+      this.todos = this.todos.filter((todoTitle) => todoTitle !== todo);
+      this.todoTitle = "";
+    },
+
+    editTodo(todo) {
+      todo.title = this.todoTitle;
+      this.todoTitle = "";
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -56,6 +83,7 @@
 .brand {
   font-size: 2rem;
 }
+
 .add-todo-form {
   display: flex;
   justify-content: space-between;
@@ -70,7 +98,7 @@
   border: none;
   height: 50px;
 }
-.todo{
+.todo {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -79,7 +107,7 @@
   padding: 0 20px 0 20px;
   border-radius: 20px;
 }
-.remove-todo-btn{
+.remove-todo-btn {
   border-radius: 50%;
   border: none;
   height: 40px;
@@ -87,5 +115,17 @@
   font-size: 30px;
   color: var(--tex-color);
   background: var(--danger-color);
+  cursor: pointer;
+}
+
+.edit-todo-btn {
+  border-radius: 50%;
+  border: none;
+  height: 40px;
+  width: 40px;
+  font-size: 30px;
+  color: var(--tex-color);
+  background: var(--edit-color);
+  cursor: pointer;
 }
 </style>
